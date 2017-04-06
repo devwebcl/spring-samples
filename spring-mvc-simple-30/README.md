4de823fa05af3e1b0d63f71816c4a75f5789eeaa
German Gonzalez <german.gonzalez@gmail.com>
2017-04-06 15:07:56 -0300

    added PropertyPlaceholderConfigurer

--- spring-mvc-simple-30/README.md
+++ spring-mvc-simple-30/README.md
@@ -2,4 +2,25 @@
 
 This is a simple Spring MVC using version 4.2.1, including different packages, such us, controller, domain, aspect, service, where each one has the respective annotation.
 
+# Spring MVC simple sample v3.0
 
+
+Este es un ejemplo simple para ver varios recursos SpringMVC y Core, que a larga componen una aplicacion SpringMVC
+
+- @Controller
+- @Autowired
+- @Value (de un .properties)
+- @RequestMapping
+- org.springframework.web.servlet.DispatcherServlet
+- mvc:annotation-driven
+- context:component-scan base-package="cl.ejemplo.presentacion"
+- bean class="org.springframework.beans.factory.config.PropertyPlaceholderConfigurer"
+- varios .properties adentro
+
+
+La idea es tener un resumen simple de diferentes funcionalidades de Spring.
+
+
+# TODO
+
+- log4j no está logeando
--- spring-mvc-simple-30/src/main/java/cl/ejemplo/presentacion/controller/HomeController.java
+++ spring-mvc-simple-30/src/main/java/cl/ejemplo/presentacion/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import cl.ejemplo.presentacion.service.IEjemploService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 
 @Controller
@@ -15,18 +17,31 @@ public class HomeController {
     @Autowired
     private IEjemploService ejemploService;
 
+    @Value("${test.scheduled}")
+    private String str;
+
+    @Value("${test.hola}")
+    private String salute;
+
+    @Value("${test.externo}")
+    private String externo;
+
 
 	@RequestMapping("/")
 	public String welcome(Model model) throws Exception  { // throws PlataformaBaseException {
 
-		model.addAttribute("greeting", "Hola Mundo !");
-		model.addAttribute("tagline", "springmvc...");
+		model.addAttribute("greeting", "Hola " + salute );
+		model.addAttribute("tagline", "springmvc... " + str);
+		model.addAttribute("meves", "me ves? " + externo);
+
 
 		//some dummy impl
 		String dummy = ejemploService.getBienvenida("welcome");
+		System.out.println(dummy);
 
 
 		return "welcome";
 	}
+
 }
 
