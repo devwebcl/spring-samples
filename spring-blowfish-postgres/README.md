cce0c70fbb736c8c92f2efa9c81119145b8299ce
German Gonzalez <german.gonzalez@gmail.com>
2017-04-12 11:25:58 -0300

    blowfish2a sample

---
+++ spring-blowfish-postgres/README.md
@@ -0,0 +1,5 @@
+# Blowfish
+
+This simple sample to hash password using blowfish2a compatible with
+Postgres pgcrypto https://www.postgresql.org/docs/9.3/static/pgcrypto.html
+
---
+++ spring-blowfish-postgres/src/main/java/cl/devweb/spring/security/hashing/blowfish/SpringSecurityTest.java
@@ -0,0 +1,34 @@
+package cl.devweb.spring.security.hashing.blowfish;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+/*
+ * This simple sample to hash password using blowfish2a compatible with
+ * Postgres pgcrypto https://www.postgresql.org/docs/9.3/static/pgcrypto.html
+ *
+ */
+public class SpringSecurityTest {
+
+	public static void main(String[] args) {
+
+		String salt = BCrypt.gensalt(6);
+		System.out.println("salt:" + salt );
+
+		String password = "holamundo";
+		String stored_hash = "$2a$06$Rja8BGNHYDAQmWdDnaPZZeZDe1PzD8AUrwwyWurJ7t7qk7SDAjjyy";
+
+		// sample from http://docs.spring.io/spring-security/site/docs/current/apidocs/org/springframework/security/crypto/bcrypt/BCrypt.html
+		String hashed = BCrypt.hashpw(password, stored_hash);
+		System.out.println("hashed-pwd:" + hashed );
+
+
+		if (BCrypt.checkpw(password, stored_hash)) {
+			System.out.println("It matches");
+		} else {
+			System.out.println("It does NOT match");
+		}
+
+	}
+
+}
+
