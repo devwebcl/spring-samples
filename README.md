d20445b1ce336a2d574cf4c0f9d5a127394792a7
unknown <german.gonzalez@gmail.com>
2021-04-20 12:02:41 -0400

    soap example

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 3. Spring Blowfish2a
 4. Spring Swagger (springfox)
 5. Atmosphere SSE Chat
+6. Spring Producing a SOAP web service
 
---
+++ gs-producing-web-service-master/complete/src/main/java/com/example/producingwebservice/ProducingWebServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// https://spring.io/guides/gs/producing-web-service/
+
+
+@SpringBootApplication
+public class ProducingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingWebServiceApplication.class, args);
+	}
+}
